e58b6f44e5d1f00912a3ad572265b4ed9bcffdae
Nico Velasquez <velasquezlopezn@gmail.com>
2022-02-23 12:56:17 -0500

    terminada la primera prueba lab definitivo, comunicación 1 sentido

--- parteESP32/parteESP32.ino
+++ parteESP32/parteESP32.ino
@@ -8,14 +8,12 @@ using namespace std;
 #define RXD2 16
 #define TXD2 17
 
-float pot1Value=0;
+float pot1Value[3] = {0, 0, 0};
 
-
-
-void recibirDato(int pot){
-  
-  
-  }
+void enviarDato(int pot) {
+  String msg = String("pot" + String(pot) + ":" + String(pot1Value[pot - 1]));
+  Serial2.println(msg);
+}
 
 void setup() {
   Serial.begin(115200);
@@ -24,27 +22,38 @@ void setup() {
   Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
 }
 
-
-
-int i=0;
+int i = 1;
 void loop() {
   if (Serial2.available() > 0) {
-      String sms = Serial2.readStringUntil('\n');
-      String label = sms.substring(0,5);
-      float value= (sms.substring(5)).toFloat();
-      
-      if (label == "pot1"){
-        
-        }
+    String sms = Serial2.readStringUntil('\n');
+    String label = sms.substring(0, 5);
+    float value = (sms.substring(5)).toFloat();
 
-      Serial.println(sms);
+    if (label == "pot1") {
+      pot1Value[0] = value;
+    }
+    if (label == "pot1") {
+      pot1Value[1] = value;
+    }
+    if (label == "pot1") {
+      pot1Value[2] = value;
+    }
+
   }
+  if (Serial.available() > 0) {
+    String sms = Serial.readStringUntil('\n');
+    Serial.println(sms);
+  }
+  
+  enviarDato(i);
+  i = i == 3 ? i = 1:i++;
 
   /*
-  pot1Value = analogRead(pinPot1);
-  Serial.print("pot1:");
-  pot1Value=3.3*pot1Value/4095;
-  Serial.println(pot1Value);
+    pot1Value = analogRead(pinPot1);
+    Serial.print("pot1:");
+    pot1Value=3.3*pot1Value/4095;
+    Serial.println(pot1Value);
   */
+  
   delay(500);
 }
